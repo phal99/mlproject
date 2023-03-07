92d2896df1bb2e28cf953c725396d2cb80533a15
phal99 <palgun.sappidi@gmail.com>
2023-03-07 16:25:41 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        for line in file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace('\n',"")for req in requirements]
+
+setup(
+name="mlproject",
+version="0.0.1",
+author="Palgun",
+author_email="palgun.sappidi@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements("requirements.txt")
+
+)
